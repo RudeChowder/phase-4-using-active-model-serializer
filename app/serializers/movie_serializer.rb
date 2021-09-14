8f533f0ec75a77d82de65f31b4d87423fbe2a088
Stephen Crane <scrane88@gmail.com>
2021-09-14 09:33:38 -0400

    done

---
+++ app/serializers/movie_serializer.rb
@@ -0,0 +1,4 @@
+require "pry"
+class MovieSerializer < ActiveModel::Serializer
+  attributes :id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director
+end
